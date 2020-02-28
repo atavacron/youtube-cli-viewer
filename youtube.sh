85c877d7c8f4707d8e4982b27741aef7a2cce0cf
getimiskon <31846052+getimiskon@users.noreply.github.com>
2020-02-28 13:51:16 +0200

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -17,40 +17,32 @@ echo "4) No video"
 echo " "
 echo "Select from 0-4:"
 read QUALITY_SELECT
-if [ "$QUALITY_SELECT" = "0" ]
-then
-	echo " "
-	echo Loading video at full quality...
+case "$QUALITY_SELECT" in
+	"0") echo " "
+	echo "Loading video at full quality..."
 	mpv "$YT_LINK"
-else
-	if [ "$QUALITY_SELECT" = 1 ]
-	then
-		echo " "
-		echo "Loading video at 720p..."
-		mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?1080]+bestaudio/best"
-	else
-		if [ "$QUALITY_SELECT" = 2 ]
-		then
-			echo " "
-			echo "Loading video at 480p..."
-			mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?720]+bestaudio/best"
-		else
-			if [ "$QUALITY_SELECT" = 3 ]
-			then
-				echo " "
-				echo "Loading video at 360p..."
-				mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?480]+bestaudio/best"
-			else
-				if [ "$QUALITY_SELECT" = 4 ]
-				then
-					echo "Loading audio..."
-					mpv "$YT_LINK" --no-video
-				else
-					echo " "
-					echo "ERROR!"
-				fi
-			fi
-		fi
-	fi
-fi
+	;;
 
+	"1") echo " "
+	echo "Loading video at 720p..."
+	mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?1080]+bestaudio/best"
+	;;
+
+	"2") echo " "
+	echo "Loading video at 480p..."
+	mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?720]+bestaudio/best"
+	;;
+
+	"3") echo " "
+	echo "Loading video at 360p..."
+	mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?480]+bestaudio/best"
+	;;
+
+	"4") echo " "
+	echo "Loading audio..."
+	mpv "$YT_LINK" --no-video
+	;;
+
+	*) echo " "
+	echo "ERROR!"
+esac
