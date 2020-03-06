5d94d76122cd3459fb31de1d28fa055408f34e48
getimiskon <31846052+getimiskon@users.noreply.github.com>
2020-03-06 11:51:51 +0200

    Uploaded the updated version of the script
    Added major changes to the code

--- youtube.sh
+++ youtube.sh
@@ -2,47 +2,53 @@
 
 #To use this script you need mpv and youtube-dl to be installed
 
-echo "CLI YouTube viewer"
-echo " "
-echo "Youtube Link: "
-read YT_LINK
-echo " "
-echo "Select quality: "
-echo " "
-echo "0) Full quality"
-echo "1) 720p"
-echo "2) 480p"
-echo "3) 360p"
-echo "4) No video"
-echo " "
-echo "Select from 0-4:"
-read QUALITY_SELECT
-case "$QUALITY_SELECT" in
-	"0") echo " "
-	echo "Loading video at full quality..."
-	mpv "$YT_LINK"
-	;;
+if [ "$1" == "help" ]
+	then
+	echo "Usage: ./youtube.sh [VIDEO LINK] [QUALITY OPTION]"
+	echo " "
+	echo "To play a video, type your link and the quality option you want to use."
+	echo " "
+	echo "Available quality options:"
+ 	echo "full-quality - Plays the video in the best quality available"
+	echo "720 - Plays the video in 720p"
+	echo "480 - Plays the video in 480p"
+	echo "360 - Plays the video in 360p"
+	echo "no-video - Plays only the audio"
+	echo " "
+	echo "To display this screen, type \"./youtube.sh help\""	
+else
+	case "$2" in
+		"full-quality") echo " "
+		echo "Loading video at full quality..."
+		echo " "
+		mpv "$1"
+		;;
 
-	"1") echo " "
-	echo "Loading video at 720p..."
-	mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?1080]+bestaudio/best"
-	;;
+		"720") echo " "
+		echo "Loading video at 720p..."
+		echo " "
+		mpv "$1" --ytdl-format="bestvideo[height<=?1080]+bestaudio/best"
+		;;
 
-	"2") echo " "
-	echo "Loading video at 480p..."
-	mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?720]+bestaudio/best"
-	;;
+		"480") echo " "
+		echo "Loading video at 480p..."
+		echo " "
+		mpv "$1" --ytdl-format="bestvideo[height<=?720]+bestaudio/best"
+		;;
+	
+		"360") echo " "
+		echo "Loading video at 360p..."
+		echo " "
+		mpv "$1" --ytdl-format="bestvideo[height<=?480]+bestaudio/best"
+		;;
+	
+		"no-video") echo " "
+		echo "Loading audio..."
+		echo " "
+		mpv "$1" --no-video
+		;;
 
-	"3") echo " "
-	echo "Loading video at 360p..."
-	mpv "$YT_LINK" --ytdl-format="bestvideo[height<=?480]+bestaudio/best"
-	;;
-
-	"4") echo " "
-	echo "Loading audio..."
-	mpv "$YT_LINK" --no-video
-	;;
-
-	*) echo " "
-	echo "ERROR!"
-esac
+		*) echo " "
+		echo "Error! Not an available option."
+	esac
+fi
